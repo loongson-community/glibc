5fd093eb072b8e50b466228f43d67a448a7dd9ef
Chenghua Xu <xuchenghua@loongson.cn>
2016-10-13 14:32:30 +0800

    Used-madd.d-in-fma-function
        Used madd.d in fma() function.
        sysdeps/mips/fpu/
        * s_fma.c: New.
        * s_fmaf.c: New.
        sysdeps/mips/mips32/fpu/
        * s_fma.c: New.
        * s_fmaf.c: New.
        sysdeps/mips/mips64/n32/fpu/
        * s_fma.c: New.
        * s_fmaf.c: New.
        sysdeps/mips/mips64/n64/fpu/
        * s_fma.c: New.
        * s_fmaf.c: New.

---
+++ sysdeps/mips/fpu/s_fma.c
@@ -0,0 +1,32 @@
+/* Compute x * y + z as ternary operation.  MIPS fused madd version.
+   Copyright (C) 2010-2014 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Contributed by Jakub Jelinek <jakub@redhat.com>, 2010.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <math.h>
+
+double
+__fma (double x, double y, double z)
+{
+  double r;
+  asm ("madd.d %0,%3,%1,%2" : "=f" (r) : "f" (x), "f" (y), "f" (z));
+  return r;
+}
+#ifndef __fma
+weak_alias (__fma, fma)
+#endif
+
